7dc61984c9afca5be9f46e75b608f063bdd52588
cai <cai.com>
2017-06-21 20:19:59 +0800

    添加任务完成

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
-(function (window) {
+﻿(function (window) {
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
+
+	//创建模板
+	var app = angular.module('todosMVC',[]);
+	app.controller('appController', ['$scope' , function ($scope) {
+		//先创建一个假数据，以后可以从后台获取
+		$scope.todos = [
+			{id:1,name:'家具',completed:false},
+			{id:2,name:'小蜗居',completed:false},
+			{id:3,name:'舒适轿车',completed:true},
+			{id:4,name:'柴米油盐',completed:false},
+			{id:5,name:'人生哲理',completed:true}
+		]
+		//添加任务
+		$scope.newTodo = '';
+		$scope.add = function () {
+			if(!$scope.newTodo){
+				return
+			}
+			$scope.todos.push({
+				id:Math.random(),name:$scope.newTodo,completed:false
+			})
+			$scope.newTodo = '';
+		}
+
+	}])
 
 })(window);
